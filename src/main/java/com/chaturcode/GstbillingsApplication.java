dfd411db3a45f472d0bc022d4fd49538485adf38
Dipak.Lokhande <dipak.lokhande@medimaze.in>
2025-05-23 13:54:29 +0530

    initial commit

---
+++ src/main/java/com/chaturcode/GstbillingsApplication.java
@@ -0,0 +1,13 @@
+package com.chaturcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstbillingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstbillingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaturcode/GstbillingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaturcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstbillingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
